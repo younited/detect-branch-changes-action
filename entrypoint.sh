9af93ce4407086d3adb7b65b63ffc07903aafff5
Jérémy Reisser <jeremy.reisser@gmail.com>
2023-05-16 17:38:10 +0200

    redirect outputs to GITHUB_OUTPUT

--- entrypoint.sh
+++ entrypoint.sh
@@ -5,17 +5,17 @@ BASE_BRANCH="remotes/origin/$1"
 PATHSPEC=${@:2}
 FORK_POINT_SHA=$(git merge-base --fork-point $BASE_BRANCH || git merge-base $BASE_BRANCH HEAD)
 
-echo ::set-output name=fork_point_sha::$FORK_POINT_SHA
+echo "fork_point_sha=$FORK_POINT_SHA" >>"$GITHUB_OUTPUT"
 
 function check() {
 
   if [[ -z "$(git diff --name-only $FORK_POINT_SHA $PATHSPEC)" ]];
   then
     echo "Detected no changes on $PATHSPEC since $FORK_POINT_SHA"
-    echo ::set-output name=changed::false
+    echo "changed=false" >>"$GITHUB_OUTPUT"
   else
     echo "Detected changes on $PATHSPEC since $FORK_POINT_SHA"
-    echo ::set-output name=changed::true
+    echo "changed=true" >>"$GITHUB_OUTPUT"
   fi
 }
 
